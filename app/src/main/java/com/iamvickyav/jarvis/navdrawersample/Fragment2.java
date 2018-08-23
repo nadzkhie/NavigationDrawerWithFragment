5f74f614882138bc6ec62d69c9f55650b13df93a
iamvickyav <vickyavw.10@gmail.com>
2018-08-24 00:17:30 +0530

    Nav Drawer with Fragments added

---
+++ app/src/main/java/com/iamvickyav/jarvis/navdrawersample/Fragment2.java
@@ -0,0 +1,16 @@
+package com.iamvickyav.jarvis.navdrawersample;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_2, container, false);
+    }
+}
---
+++ app/src/main/java/com/iamvickyav/jarvis/navdrawersample/MainActivity.java
@@ -0,0 +1,62 @@
+package com.iamvickyav.jarvis.navdrawersample;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.my_drawer);
+        navigationView = findViewById(R.id.nav_view);
+        textView = findViewById(R.id.textView);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+               /* menuItem.setChecked(true);
+                textView.setText(menuItem.getTitle());
+                drawerLayout.closeDrawers();
+                return true;*/
+                Fragment fragment = null;
+                switch (menuItem.getItemId()) {
+
+                    case R.id.nav_account:
+                        fragment = new Fragment1();
+                        break;
+                    case R.id.nav_gallery:
+                        fragment = new Fragment2();
+                        break;
+                    case R.id.nav_settings:
+                        fragment = new Fragment3();
+                        break;
+                }
+
+                if (fragment != null) {
+                    FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                    ft.replace(R.id.content_frame, fragment);
+                    ft.commit();
+                }
+
+                menuItem.setChecked(true);
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+}
